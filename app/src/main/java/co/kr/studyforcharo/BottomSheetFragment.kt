4cccfcd935835be4e0a3ead28bbb1dc459291eef
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-10-04 03:16:34 +0900

    [FIX] 높이 이상한 버그 해결

--- app/src/main/java/co/kr/studyforcharo/BottomSheetFragment.kt
+++ app/src/main/java/co/kr/studyforcharo/BottomSheetFragment.kt
@@ -1,26 +1,35 @@
 package co.kr.studyforcharo
 
-import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import co.kr.studyforcharo.databinding.BottomSheetDialogBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
+
 class BottomSheetFragment : BottomSheetDialogFragment() {
+    lateinit var binding: BottomSheetDialogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_dialog, container, false)
+    ): View {
+        binding = BottomSheetDialogBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //이부분임
+        binding.clBds.layoutParams.height =
+            requireActivity().windowManager.currentWindowMetrics.bounds.height()
         (dialog as BottomSheetDialog).behavior.apply {
             isFitToContents = false
             state = BottomSheetBehavior.STATE_HALF_EXPANDED
